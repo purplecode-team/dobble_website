416d26a11415c7ee77cd911cfe7f341f27e56be7
naerim <naerim1119@gmail.com>
2020-10-30 19:27:24 +0900

    [SET] styled-components [ADD] AppLayout

---
+++ src/components/AppLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+
+      <div>{children}</div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default AppLayout;
