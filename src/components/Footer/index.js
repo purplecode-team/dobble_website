0e67200321cb4ac6467493916c6ef1530c04716a
annie1004619 <annie1004619@aver.com>
2020-11-07 14:29:51 +0900

    [SET] styled-components [DEL] mypage

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import SubscribeMagazine from './subscribeMagazine';
+import InputEmail from './inputEmail';
+import Sns from './sns';
+import Dot from './dot';
+import Info from './info';
 
 const Footer = () => {
-  return <div>footer page</div>;
+  return (
+    <div>
+      <SubscribeMagazine />
+      <InputEmail />
+      <Sns />
+      <Dot />
+      <Info />
+    </div>
+  );
 };
 
 export default Footer;
