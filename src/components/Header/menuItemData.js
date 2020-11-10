aa66f693fda88f91d4d6fd6e523dd51420d95fcd
naerim <naerim1119@gmail.com>
2020-11-10 14:47:25 +0900

    [ADD] menuItem data

---
+++ src/components/Header/menuItemData.js
@@ -0,0 +1,78 @@
+export const ProductsData = [
+  {
+    key: 1,
+    content: '의류',
+    link: '/',
+  },
+  {
+    key: 2,
+    content: '가방',
+    link: '/',
+  },
+  {
+    key: 3,
+    content: '악세서리',
+    link: '/',
+  },
+  {
+    key: 4,
+    content: '잡화',
+    link: '/',
+  },
+];
+
+export const BrandsData = [
+  {
+    key: 1,
+    content: '마리몬드',
+    link: '/',
+  },
+  {
+    key: 2,
+    content: '마르코로호',
+    link: '/',
+  },
+  {
+    key: 3,
+    content: '애니휴먼',
+    link: '/',
+  },
+  {
+    key: 4,
+    content: '위드아이스',
+    link: '/',
+  },
+  {
+    key: 5,
+    content: 'BOCE',
+    link: '/',
+  },
+  {
+    key: 6,
+    content: '메리디아니',
+    link: '/',
+  },
+  {
+    key: 7,
+    content: '뉴킷',
+    link: '/',
+  },
+  {
+    key: 8,
+    content: '파이어마커스',
+    link: '/',
+  },
+];
+
+export const MagazineData = [
+  {
+    key: 1,
+    content: '브랜드 소개',
+    link: '/',
+  },
+  {
+    key: 2,
+    content: '기부 소식',
+    link: '/',
+  },
+];
