babdb3facc5216f8c81352a13620a00abf4fb945
annie1004619 <annie1004619@naver.com>
2020-12-04 02:42:34 +0900

    [ADD] firebase 추가

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPTz-FtrBKKF8LPmTloynxayC7RtGJus4',
+  authDomain: 'doble-d08a9.firebaseapp.com',
+  databaseURL: 'https://doble-d08a9.firebaseio.com',
+  projectId: 'doble-d08a9',
+  storageBucket: 'doble-d08a9.appspot.com',
+  messagingSenderId: '670070936445',
+  appId: '1:670070936445:web:349fe2e62a25feb874f214',
+  measurementId: 'G-NWDRGD0XJE',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+export default firebase;
