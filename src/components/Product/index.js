a7a45d68383d15e72158c5d6999d35b85eff00ea
naerim <naerim1119@gmail.com>
2020-12-12 14:13:53 +0900

    [ADD] Product dummyData

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import PLayout from './PLayout';
+import { dummyData } from '../dummyData';
 
 const Product = () => {
   return (
@@ -14,11 +15,68 @@ const Product = () => {
         </Desc>
       </Banner>
       <PLayout>
-        <div>page</div>
+        {dummyData.map(({ alt, title, link, img, price, brand, banner }) => (
+          <ProductDiv key={alt} href={link}>
+            <ProductImgDiv>
+              <ProductImg src={img} alt={alt} />
+            </ProductImgDiv>
+            <Text>{brand}</Text>
+            <Text>
+              <BannerText>{banner}</BannerText>
+              {title}
+            </Text>
+            <Price>{price}</Price>
+          </ProductDiv>
+        ))}
       </PLayout>
     </div>
   );
 };
+
+const ProductDiv = styled.div`
+  display: inline-block;
+  margin-right: 30px;
+  margin-bottom: 30px;
+  padding: 10px;
+  padding-top: 0;
+  @media (max-width: 768px) {
+    margin-right: 0;
+  }
+`;
+
+const ProductImgDiv = styled.div`
+  width: 220px;
+  height: 300px;
+  overflow: hidden;
+  border-radius: 18px 18px;
+  background: rgb(220, 220, 220);
+`;
+
+const ProductImg = styled.img`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
+
+const Text = styled.div`
+  font-size: 0.8rem;
+  margin: 10px 0;
+`;
+
+const BannerText = styled.div`
+  background: yellow;
+  font-size: 0.8rem;
+  color: rgb(100, 100, 100);
+  display: inline-block;
+  margin-right: 5px;
+`;
+
+const Price = styled.div`
+  font-weight: bold;
+  font-size: 2rem;
+`;
 
 const Banner = styled.div`
   height: 80vh;
