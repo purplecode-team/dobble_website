c7325bc96e4bf5bf5de6c384cf44285b5527aae7
annie1004619 <annie1004619@naver.com>
2020-12-19 00:52:47 +0900

    [FIX] loading과 empty일 때 , '/brand' 경로 일때

--- src/hooks/useList.js
+++ src/hooks/useList.js
@@ -5,33 +5,55 @@ export default (initialUrl) => {
   const [value, setValue] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [empty, setEmpty] = useState(false);
 
   /* 현재 클릭한 경로에 따라서 데이터베이스에 저장된 메거진 중 해당 경로의 카테고리에 해당하는 것만 나타낸다 */
   useEffect(() => {
     const dataArray = [];
     const currentCategory = initialUrl.params.category;
+
     /* 데이터베이스에 'magazine','product' 테이블에 데이터가 저장되어 있으므로 경로 중 'magazine' 'product'를 빼서 ref에 넣어준다 */
     const pathArray = initialUrl.path.split('/');
-    const query = firebase.database().ref(pathArray[1]).orderByKey();
 
-    try {
-      setLoading(true);
-
-      query.once('value').then(function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-          const childData = childSnapshot.val();
-          if (currentCategory === undefined || childData.category === currentCategory) {
-            dataArray.push(childData);
-          }
-        });
-        setValue(dataArray);
-      });
-      setLoading(false);
-    } catch (_error) {
-      setLoading(false);
-      setError(_error);
+    if (pathArray[1] === 'brand') {
+      pathArray[1] = 'product';
     }
+
+    const query = firebase.database().ref(pathArray[1]).orderByKey();
+    const loadData = async () => {
+      try {
+        setLoading(true);
+
+        await query.once('value').then(function (snapshot) {
+          snapshot.forEach(function (childSnapshot) {
+            const childData = childSnapshot.val();
+
+            if (currentCategory === undefined || childData.category === currentCategory) {
+              dataArray.push(childData);
+            } else if (childData.brand === currentCategory) {
+              dataArray.push(childData);
+            }
+          });
+          setValue(dataArray);
+        });
+        setLoading(false);
+      } catch (_error) {
+        setLoading(false);
+        setError(_error);
+      }
+    };
+    loadData();
   }, [initialUrl.params.category]);
 
-  return [value, error, loading];
+  useEffect(() => {
+    if (loading === true) {
+      setEmpty(false);
+    } else if (value.length === 0) {
+      setEmpty(true);
+    } else {
+      setEmpty(false);
+    }
+  }, [value, loading]);
+
+  return [value, error, loading, empty];
 };
